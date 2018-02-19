0290e2d67be5343305b20d6e0b0565d093a272f2
Jaime Alejandro Perez <jperezh3@gmail.com>
2018-02-19 21:36:35 +0100

    Fix this

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const app = express();
 const port = 80;
 
 app.get('/', (request, response) => {
-	response.sendFile(path.join(__dirname, '/oldWebsite/home.html'));
+	response.sendFile(path.join(__dirname, '/home.html'));
 });
-app.use(favicon(path.join(__dirname, 'oldWebsite', 'favicon.ico')))
-app.use(express.static(__dirname + '/oldWebsite/assets'));
+app.use(favicon(path.join(__dirname, 'favicon.ico')))
+app.use(express.static(__dirname + '/assets'));
 
 app.get('*', (request, response) => {
 	response.sendFile(path.join(__dirname, '/404.html'));
