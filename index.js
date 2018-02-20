a9fda4e0966a3a0fd0d02c3c624b9cb9e3fd41e1
Jaime Alejandro Perez <jperezh3@gmail.com>
2018-02-20 19:49:10 +0100

    W :x

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-const express = require('express');
-const path = require('path');
-const favicon = require('serve-favicon');
-
-const app = express();
-const port = 80;
-
-app.get('/', (request, response) => {
-	response.sendFile(path.join(__dirname, '/home.html'));
-});
-
-app.use(favicon(path.join(__dirname, '/favicon.ico')));
-
-app.use(express.static(__dirname + '/assets'));
-
-app.get('*', (request, response) => {
-	response.sendFile(path.join(__dirname, '/404.html'));
-});
-
-app.listen(port, () => {
-	console.log("App has started listening on the port: " + port);
-});
+console.log("WHAT THE FUCK");
+// const express = require('express');
+// const path = require('path');
+// const favicon = require('serve-favicon');
+//
+// const app = express();
+// const port = 80;
+//
+// app.get('/', (request, response) => {
+// 	response.sendFile(path.join(__dirname, '/home.html'));
+// });
+//
+// app.use(favicon(path.join(__dirname, '/favicon.ico')));
+//
+// app.use(express.static(__dirname + '/assets'));
+//
+// app.get('*', (request, response) => {
+// 	response.sendFile(path.join(__dirname, '/404.html'));
+// });
+//
+// app.listen(port, () => {
+// 	console.log("App has started listening on the port: " + port);
+// });
